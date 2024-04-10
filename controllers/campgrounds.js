f0aeb18e292636db7fb700cd3cb2421ba856da3b
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-04-10 23:16:17 +0900

    画像の追加と削除の機能を追加

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -51,6 +52,7 @@ module.exports.renderEditForm = async (req, res) => {
 };
 
 module.exports.updateCampground = async (req, res) => {
+  console.log(req.body);
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
@@ -62,7 +64,10 @@ module.exports.updateCampground = async (req, res) => {
   campground.images.push(...imgs);
   await campground.save();
   if (req.body.deleteImages) {
-    await campground.update0ne({
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
       $pull: { images: { filename: { $in: req.body.deleteImages } } },
     });
   }
