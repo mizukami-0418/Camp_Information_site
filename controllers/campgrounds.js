e3b025e4f2987ba769c0425712e17d8b6e539f6f
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-04-10 21:48:00 +0900

    ignoreファイルを修正しリモートを再構築

---
+++ controllers/campgrounds.js
@@ -0,0 +1,78 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("campgrounds/new");
+};
+
+module.exports.showCampground = async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+    // .populate("reviews")
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
+  console.log(campground);
+  if (!campground) {
+    req.flash("error", "キャンプ場が見つかりませんでした");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
+
+module.exports.createCampground = async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.author = req.user._id;
+  await campground.save();
+  console.log(campground);
+  req.flash("success", "新しいキャンプ場を登録しました！！");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "キャンプ場が見つかりませんでした");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
+
+module.exports.updateCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  const imgs = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.images.push(...imgs);
+  await campground.save();
+  if (req.body.deleteImages) {
+    await campground.update0ne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
+  req.flash("success", `キャンプ場「${campground.title}」を更新しました`);
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "キャンプ場を削除しました");
+  res.redirect("/campgrounds");
+};
