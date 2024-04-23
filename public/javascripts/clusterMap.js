eebb29f16d3121bf69be93f9f035e0155e30e75e
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-04-23 21:39:01 +0900

    インラインスタイルの修正

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   style: "mapbox://styles/mapbox/streets-v12",
   center: [138, 39],
   zoom: 3,
